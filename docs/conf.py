c352ea78b2b110c4156458ae90c24a0948ff095b
Michal-Babins <mbabins5@uoregon.edu>
2022-01-11 14:51:28 -0700

    fixed theme

--- docs/conf.py
+++ docs/conf.py
@@ -33,7 +33,7 @@ release = '0.1'
 extensions = ['m2r2']
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+# templates_path = ['_templates']
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -46,7 +46,7 @@ source_suffix = ['.rst', '.md']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'classic'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
