4136f83290112ee6930a359334ee366906e9ce31
Yan Xu <yxu@pn1704854.lanl.gov>
2024-02-27 10:27:35 -0800

    fix markdown table

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,8 @@ release = '0.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-   'recommonmark'
+   'recommonmark',
+   'sphinx_markdown_tables'
 ]
 
 
